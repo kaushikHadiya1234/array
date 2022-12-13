722f54eee5cdcc1d1a05fc2249b3f5a1bd8d3511
kaushikHadiya1234 <119835333+kaushikHadiya1234@users.noreply.github.com>
2022-12-13 11:18:02 +0530

    Add files via upload

--- 13-12-22/pr5q4.c
+++ 13-12-22/pr5q4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-	int i,j,n,a[100],min,mxi;
+	int i,j,n,a[100],min,mxi=0;
 	
 	printf("enter the size of array");
 	scanf("%d",&n);
@@ -11,23 +11,22 @@ main()
 		scanf("%d",&a[i]);
 	}
 	
-	mxi=a[0];
-	for(i=0;i<n;i++)
-	{`
-		if(a[i]>mxi)
-		{
-			printf("mximum number is = %d",a[i]);
-	    }
-	    
- }
 	min=a[0];
 	for(i=0;i<n;i++)
 	{
-		if(a[i]<min)
+		if(a[i]>=mxi)
 		{
-			printf("minimum number is = %d",a[i]);
+		    mxi=a[i];
+	    }
+	    
+		if(a[i]<=min)
+		{
+		    min=a[i];
 		}
 		
 	}
+	
+		printf("mximum number is = %d\n",mxi);
+		printf("minimum number is = %d",min);
 
 }
